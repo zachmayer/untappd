a65c931b8b7f0ff7af9de2b701d163b3a01a106a
Zachary Deane-Mayer <zach.mayer@gmail.com>
2016-09-28 13:44:59 -0400

    last night's attempt at recs

--- R/ConnectToUntappd.R
+++ R/ConnectToUntappd.R
@@ -9,38 +9,64 @@ load_config_file <- function(config_path=get_default_config_path()) {
   )
 }
 
-get_user_checkins <- function(user='zachmayer86', config=getOption('untappd_config')){
-  response <- httr::GET(
-    config$endpoint,
-    path=paste0('/v4/user/checkins/', user),
-    query=list(
-      client_id = config$client_id,
-      client_secret = config$client_secret,
-      access_token = config$access_token
-    )
-  )
-  stop_for_status(response)
-  checkins = content(response)[[3]]$checkins$items
-  return(checkins)
-}
-
-parse_checkins = function(c){
-  out <- pbapply::pblapply(c, function(x){
+parse_checkins <- function(c){
+  out <- lapply(c, function(x){
     data.table::data.table(
       rating = x$rating_score,
-      cid = x$checkin_id,
-      ts = x$created_at,
-      uid = x$user$uid,
-      bid = x$beer$bid,
+      user_id = x$user$uid,
+      beer_id = x$beer$bid,
+      brewery_id = x$brewery$brewery_id,
+      checkin_id = x$checkin_id,
+      time = x$created_at,
+      brewery_name = x$brewery$brewery_name,
+      beer_name = x$beer$beer_name,
       abv = x$beer$beer_abv
     )
   })
   out <- data.table::rbindlist(out)
-  data.table::setkeyv(out, 'cid')
+  data.table::setkeyv(out, 'checkin_id')
   return(out)
 }
 
-#
-# https://api.untappd.com/v4/user/checkins?client_id=ID&client_secret=SEC&access_token=TOKEN
-# Get token:
-# #https://untappd.com/oauth/authenticate/?client_id=ID&response_type=token&redirect_url=http://moderntoolmaking.blogspot.com/
+get_checkins <- function(
+  type = 'user',
+  id = 'zachmayer86',
+  wait = 5,
+  n=100,
+  record_per_page=25,
+  config=getOption('untappd_config')){
+
+  calls <- ceiling(n/record_per_page)
+  max_id <- NULL
+  checkin_list <- list()
+
+  pb <- txtProgressBar(min=0, max=calls, style=3, char='+')
+  for(i in 1:calls){
+    response <- httr::GET(
+      config$endpoint,
+      path=paste0('/v4/', type, '/checkins/', id),
+      query=list(
+        client_id = config$client_id,
+        client_secret = config$client_secret,
+        access_token = config$access_token,
+        max_id = max_id
+      )
+    )
+    httr::stop_for_status(response)
+    content <- httr::content(response)
+    checkins <- content[['response']]$checkins$items
+    checkins <- parse_checkins(checkins)
+    checkin_list <- c(checkin_list, list(checkins))
+
+    setTxtProgressBar(pb, i)
+    if(nrow(checkins) < record_per_page){
+      break
+    }else{
+      max_id <- content[['response']][['pagination']][['max_id']]
+    }
+    Sys.sleep(wait)
+  }
+  res <- data.table::rbindlist(checkin_list)
+  return(res)
+  return(checkins)
+}
--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 untappd
 ==============
 
-R wrapper for the [Untappd v4 API](https://untappd.com/api/docs/v4).
+R package for grabbing data from [Untappd v4 API](https://untappd.com/api/docs/v4) and making beer recommendations for a specific user.
 
-Based almost entirely on the excellent [untappd-python](https://github.com/cbetz/untappd-python).
+Somewhat based on the excellent [untappd-python](https://github.com/cbetz/untappd-python).
+
+todo
+==============
+- function to get an API toke, given client secret and client id
+- function to write a config file, given client secret and client id
+- function to set the config options, given client secret and client id
+- reccomendation functions
